9d9fc3ec00d61da91227f9c883c963cb81a2c820
Mehmetcil <mehmetcil096@gmail.com>
2022-11-18 23:36:25 +0300

    ders39

--- src/ders37_exceptions/C07_Trycatch.java
+++ src/ders37_exceptions/C07_Trycatch.java
@@ -1,2 +1,19 @@
-package ders37_exceptions;public class C07_Trycatch {
+package ders37_exceptions;
+
+public class C07_Trycatch {
+
+    public static void main(String[] args) {
+
+        try {
+            calis();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void calis() {
+        calis();
+        throw new RuntimeException ("Çoook çalış");
+    }
+
 }
---
+++ src/ders39_AbstractClass/KChildClass.java
@@ -0,0 +1,7 @@
+package ders39_AbstractClass;
+
+public class KChildClass {
+
+    // Bir class birden fazla class'a extends EDILEMEZ
+    // Ancak birden fazla Interface'i implements EDEBILIR
+}
